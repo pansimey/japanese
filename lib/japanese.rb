c879a37b77933591c5baca287015b37e3615aeee
Hajime WAKAHARA <hajime.wakahara@gmail.com>
2011-08-11 12:47:59 +0900

    all pos and planed super class

--- lib/japanese.rb
+++ lib/japanese.rb
@@ -14,10 +14,122 @@ module Japanese
     end
   end
 
-  module Conjugatable
+  class OpenWord < PosString
+  end
+
+  class ClosedWord < PosString
+    def +(latter)
+      if latter.is_a(self.class)
+      else
+        super
+      end
+    end
+  end
+
+  class ClosedWordPhrase < ClosedWord
+  end
+
+  class Adj < OpenWord
+    class ClosedAdj < ClosedWord
+    end
+  end
+
+  class Adnominal < OpenWord
+  end
+
+  class Adverb < OpenWord
+  end
+
+  class Auxil < ClosedWord
+    def initialize(string, cform)
+      @connectable_form = cform
+      super(string)
+    end
+    attr_reader :connectable_form
+  end
+
+  class Conjunction < ClosedWord
+  end
+
+  class Interjection < OpenWord
+  end
+
+  class Noun < OpenWord
+    class Adjv < OpenWord
+    end
+
+    class Adverbal < OpenWord
+    end
+
+    class Demonst < OpenWord
+    end
+
+    class Nai < OpenWord
+    end
+
+    class Name < OpenWord
+    end
+
+    class Number < OpenWord
+    end
+
+    class Org < OpenWord
+    end
+
+    class Others < ClosedWord
+    end
+
+    class Place < OpenWord
+    end
+
+    class Proper < OpenWord
+    end
+
+    class Verbal < OpenWord
+    end
+  end
+
+  class Others < ClosedWord
+  end
+
+  class PostpCol < ClosedWord
+  end
+
+  class Postp < ClosedWord
+  end
+
+  class Prefix < ClosedWord
+    def initialize(string, pos)
+      @pos = pos
+      super(string)
+    end
+  end
+
+  class Suffix < ClosedWord
+    def initialize(string, pos)
+      @pos = pos
+      super(string)
+    end
+  end
+
+  class Symbol < ClosedWord
+  end
+
+  class Verb < OpenWord
+    class ClosedVerb < ClosedWord
+    end
+    file_dir = File.dirname(File.expand_path(__FILE__))
+    conf_path = file_dir + '/japanese/verb.yml'
+    @@ending = YAML.load_file(conf_path)
+    def initialize(string, ctype)
+      @ctype = ctype
+      @ending = @@ending[ctype]
+      super
+    end
+
     def conjugate(form)
       raise "form '#{form}' is not defined" unless @ending.key?(form)
-      self.sub(/#{@ending['基本形']}$/u){''} + @ending[form]
+      self.sub(/#{@ending['基本形']}$/, '') + @ending[form]
     end
 
     def +(latter)
@@ -26,23 +138,6 @@ module Japanese
       else
         super
       end
-    end
-
-    private
-    def connect(latter)
-      conjugate(latter.connectable_form) + latter
-    end
-  end
-
-  class Verb < PosString
-    file_dir = File.dirname(File.expand_path(__FILE__))
-    conf_path = file_dir + '/japanese/verb.yml'
-    @@ending = YAML.load_file(conf_path)
-    include Conjugatable
-    def initialize(string, ctype)
-      @ctype = ctype
-      @ending = @@ending[ctype]
-      super(string)
     end
 
     private
@@ -62,13 +157,5 @@ module Japanese
       rendaku_map = {'た' => 'だ', 'ち' => 'じ', 'て' => 'で', 'と' => 'ど'}
       latter.sub(/^./, rendaku_map)
     end
-  end
-
-  class AuxiliaryVerb < PosString
-    def initialize(string, cform)
-      @connectable_form = cform
-      super(string)
-    end
-    attr_reader :connectable_form
   end
 end
--- spec/japanese_spec.rb
+++ spec/japanese_spec.rb
@@ -1,7 +1,9 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Japanese" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe Japanese::PosString do
+  subject{ Japanese::PosString.new('string') }
+  it{ should be_frozen }
+  it 'dup of a PosString should be a String' do
+    subject.dup.should be_a String
   end
 end
