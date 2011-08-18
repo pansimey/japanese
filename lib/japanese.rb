118dddb7524a1f8a140fdeb143465c50ebc83b5f
Hajime WAKAHARA <hajime.wakahara@gmail.com>
2011-08-18 20:37:36 +0900

    remove some POSes

--- lib/japanese.rb
+++ lib/japanese.rb
@@ -19,7 +19,7 @@ module Japanese
 
   class ClosedWord < PosString
     def +(latter)
-      if latter.is_a(self.class)
+      if latter.is_a(CloseWord)
       else
         super
       end
@@ -29,73 +29,7 @@ module Japanese
   class ClosedWordPhrase < ClosedWord
   end
 
-  class Adj < OpenWord
-    class ClosedAdj < ClosedWord
-    end
-  end
-
-  class Adnominal < OpenWord
-  end
-
-  class Adverb < OpenWord
-  end
-
-  class Auxil < ClosedWord
-    def initialize(string, cform)
-      @connectable_form = cform
-      super(string)
-    end
-    attr_reader :connectable_form
-  end
-
-  class Conjunction < ClosedWord
-  end
-
-  class Interjection < OpenWord
-  end
-
   class Noun < OpenWord
-    class Adjv < OpenWord
-    end
-
-    class Adverbal < OpenWord
-    end
-
-    class Demonst < OpenWord
-    end
-
-    class Nai < OpenWord
-    end
-
-    class Name < OpenWord
-    end
-
-    class Number < OpenWord
-    end
-
-    class Org < OpenWord
-    end
-
-    class Others < ClosedWord
-    end
-
-    class Place < OpenWord
-    end
-
-    class Proper < OpenWord
-    end
-
-    class Verbal < OpenWord
-    end
-  end
-
-  class Others < ClosedWord
-  end
-
-  class PostpCol < ClosedWord
-  end
-
-  class Postp < ClosedWord
   end
 
   class Prefix < ClosedWord
@@ -110,9 +44,6 @@ module Japanese
       @pos = pos
       super(string)
     end
-  end
-
-  class Symbol < ClosedWord
   end
 
   class Verb < OpenWord
